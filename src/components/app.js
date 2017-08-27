2b99a1a7f5e26fc3a8dc4d5a1dc98aa7f4252587
Jaime Rios <jaimemadrigalrios@gmail.com>
2017-08-27 00:07:47 -0500

    Added video detail.

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './search_bar';
 import VideoList from './video_list';
+import VideoDetail from './video_detail';
 const API_KEY = 'AIzaSyDF4VI-2QnolYiN1Ic3cu4QE01MLWFok7o';
 
 export default class App extends Component {
@@ -19,6 +20,7 @@ export default class App extends Component {
     return (
       <div><h1>My awesome App</h1>
 			<SearchBar />
+			<VideoDetail video={this.state.videos[0]}/>
 			<VideoList videos={this.state.videos} />  
       </div>
     );
--- src/components/video_list-item.js
+++ src/components/video_list-item.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 
-export default class VideoListItem extends Component {
-	render() {
-		return (
-			<li>Video</li>
-		);
-	}
+const VideoListItem = ({ video }) => {
+	return (
+		<li className="list-group-item">
+			<div className="video-list media">
+				<div className="media-left">
+					<img src={video.snippet.thumbnails.default.url} alt="Video Thumbnail" className="media-object" />
+				</div>
+
+				<div className="media-body">
+					<div className="media-heading">{video.snippet.title}
+					</div>
+				</div>
+			</div>
+		</li>
+	);
 }
+
+export default VideoListItem;
