cdd53b2720b6685c4f14a38552b2dc6d3a15e1fb
Jaime Rios <jaimemadrigalrios@gmail.com>
2017-08-27 19:55:19 -0500

    Added loadash debounce.

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './search_bar';
 import VideoList from './video_list';
@@ -27,6 +28,8 @@ export default class App extends Component {
 	}
 
   render() {
+		const videoSearch = _.debounce((term) =>  this.videoSearch(term), 300);
+
     return (
       <div><h1>My awesome App</h1>
 			<SearchBar onSearchTermChange={ term => this.videoSearch(term) } />
